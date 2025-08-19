940954ff4623bf2f8e81b390946fb0dfdfe06881
Selms4 <selma.memic@edu.fit.ba>
2025-08-19 02:20:22 +0200

    Fixing

--- script.js
+++ script.js
@@ -75,10 +75,3 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
-// Form submission
-const form = document.querySelector('form');
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    alert('Thank you for your message! I will get back to you soon.');
-    form.reset();
-});
